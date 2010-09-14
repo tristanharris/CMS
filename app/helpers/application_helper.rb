c68fa27d1dbcad4f5a740ca549e91ecf1454d319
Tristan Harris <tris.dev@pumpcottage.me.uk>
2010-09-14 22:18:18 +0100

    Explorer theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@
 module ApplicationHelper
   include Refinery::ApplicationHelper # leave this line in to include all of Refinery's core helper methods.
 
+
+  def breadcrumbs(page=@page)
+    return [Page.find_by_link_url('/')] if page.nil?
+    breadcrumbs(page.parent) << page
+  end
+
+  def logo(page=@page)
+    return Page.find_by_link_url('/')[:logo] if page.nil?
+    return page[:logo] unless page[:logo].nil?
+    logo(page.parent)
+  end
 end
